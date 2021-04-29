0e62473246b7f59c5e5fbc74cfe7b2e732a3e074
admin <admin@YD02-17>
2021-04-29 13:24:36 +0900

    2021-04-29

--- src/ajax/MemberServlet.java
+++ src/ajax/MemberServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import common.DBcon;
 
-@WebServlet("/jquery/memberServlet.do") //실제 주소
+@WebServlet("/jquery/memberServlet.do") // 실제 주소
 public class MemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -30,38 +30,63 @@ public class MemberServlet extends HttpServlet {
 
 		// 조회 sql
 		Connection conn = DBcon.getConnect();
-		Statement stmt;
-		ResultSet rs;
-		PreparedStatement psmt;
-		
-		String p1 = request.getParameter("m_id");
-		String p2 = request.getParameter("m_name");
-		String p3 = request.getParameter("m_age");
-		
-		String sql = "select * from member";
+		ResultSet rs = null;
+		PreparedStatement psmt = null;
+		String json = "[";
+		String sql = "select member_id, member_name, member_age from member";
 		try {
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery(sql);
-		} catch (Exception e) {
-			// TODO: handle exception
+			psmt = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			rs = psmt.executeQuery();
+			while (rs.next()) {
+				String memId = rs.getString(1);
+				String memName = rs.getString(2);
+				int memAge = rs.getInt(3);
+				
+				// 조회 결과를 json 형식으로 변환 [
+				// {"id":1,"name":"hong","age":20}
+				// {"id":1,"name":"hong","age":20}
+				// {"id":1,"name":"hong","age":20}]
+				json += "{\"Id\":\"" + memId + "\"" + ",\"Name\":\"" + memName + "\"" + ",\"Age\":" + memAge + "}";
+				if (!rs.isLast())
+					json += ",";
+			}
+			json += "]";
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (psmt != null)
+				try {
+					psmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if (conn != null)
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		}
-		// 조회 결과를 json 형식으로 변환 [
-			// 							{"id":1,"name":"hong","age":20}
-			// 							{"id":1,"name":"hong","age":20}
-			// 							{"id":1,"name":"hong","age":20}]
-			
 		// 결과를 response.getWriter().print(); 출력
-
-		response.getWriter().print("<h2>정상적으로 조회되었습니다.</h2>");
+		response.getWriter().print(json);
 	}
 
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String p1 = request.getParameter("m_id");
 		String p2 = request.getParameter("m_name");
 		String p3 = request.getParameter("m_age");
-		
+
 		Connection conn = DBcon.getConnect();
 		PreparedStatement psmt = null;
 		String sql = "insert into member values(?,?,?)";
@@ -74,15 +99,15 @@ public class MemberServlet extends HttpServlet {
 			System.out.println(r + "건 입력 완료");
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally {
-			if(psmt != null)
+		} finally {
+			if (psmt != null)
 				try {
 					psmt.close();
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			if(conn != null)
+			if (conn != null)
 				try {
 					conn.close();
 				} catch (SQLException e) {
@@ -90,8 +115,9 @@ public class MemberServlet extends HttpServlet {
 					e.printStackTrace();
 				}
 		}
-		response.getWriter().print("<h2>정상적으로 입력되었습니다</h2>");
-		
+		// {"id":1, "name":"hong", "age":20}
+		String json = "{\"id\":\""+p1+"\",\"name\":\""+p2+"\",\"age\":\""+p3+"\"}";
+		response.getWriter().print(json);
 	}
 
 }
