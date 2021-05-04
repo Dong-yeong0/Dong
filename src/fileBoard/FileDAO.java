eb6e090484f84bea2badd6cdd74c589144314529
admin <admin@YD02-17>
2021-05-04 13:29:23 +0900

    2021-05-04

---
+++ src/fileBoard/FileDAO.java
@@ -0,0 +1,108 @@
+package fileBoard;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import common.DBcon;
+
+public class FileDAO {
+	Connection conn;
+	PreparedStatement psmt;
+	ResultSet rs;
+	
+	public List<FileVO> getFileList(){
+		conn = DBcon.getConnect();
+		List<FileVO> list = new ArrayList<FileVO>();
+		try {
+			psmt = conn.prepareStatement("select * from file_board");
+			rs = psmt.executeQuery();
+			while(rs.next()) {
+				FileVO vo = new FileVO();
+				vo.setNum(rs.getInt("num"));
+				vo.setAuthor(rs.getString("author"));
+				vo.setDay(rs.getString("day"));
+				vo.setTitle(rs.getString("title"));
+				vo.setFileName(rs.getString("file_name"));
+				list.add(vo);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return list;
+	}
+	
+	
+	
+	public FileVO getInsertKeyVal(FileVO vo) {
+		conn = DBcon.getConnect();
+		// 신규번호, 한건 입력, 한건조회
+		String selectKey = "select nvl(max(num),0)+1 from file_board";
+		String insertSql = "insert into file_board values(?,?,?,?,to_char(sysdate, 'YYYY-MM-DD'))";
+		String selectSql = "select * from file_board where num = ?";
+		FileVO file = new FileVO();
+		int key = 0;
+		try {
+			// 키 값을 가져오는 부분
+			psmt = conn.prepareStatement(selectKey);
+			rs = psmt.executeQuery();
+			if(rs.next()) {
+				key = rs.getInt(1);
+			}
+			// 새로운 키 값으로 한 건 입력
+			psmt = conn.prepareStatement(insertSql);
+			psmt.setInt(1, key);
+			psmt.setString(2, vo.getAuthor());
+			psmt.setString(3, vo.getTitle());
+			psmt.setString(4, vo.getFileName());
+			int r = psmt.executeUpdate();
+			System.out.println(r + "건 입력.");
+			
+			// 신규 입력된 전체 정보를 가져온다
+			psmt = conn.prepareStatement(selectSql);
+			psmt.setInt(1, key);
+			rs = psmt.executeQuery();
+			if(rs.next()) {
+				file.setAuthor(rs.getString("author"));
+				file.setDay(rs.getString("day"));
+				file.setFileName(rs.getString("file_name"));
+				file.setNum(rs.getInt("num"));
+				file.setTitle(rs.getString("title"));
+			}
+			} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		
+		return file;
+	}
+	public void close() {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if(psmt != null) {
+			try {
+				psmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
--- src/fileBoard/FileUploadServlet.java
+++ src/fileBoard/FileUploadServlet.java
@@ -1,4 +1,4 @@
-package ajax;
+package fileBoard;
 
 import java.io.IOException;
 import java.util.Enumeration;
@@ -10,59 +10,73 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
-@WebServlet("/jquery/uploadServlet")
-public class UploadServlet extends HttpServlet {
+import PlugIn.JsonObject;
+
+@WebServlet("/fileUploadServlet")
+public class FileUploadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public UploadServlet() {
+	public FileUploadServlet() {
 		super();
 	}
 
-	@Override
-	public void init() throws ServletException {
-		System.out.println("init call()"); // 최조로 한번만 실행
-	}
-
-//	@Override
-//	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		System.out.println("service call()"); //get post 상관없이 호출 
-//		Enumeration<String> enumer = req.getHeaderNames();
-//		while(enumer.hasMoreElements()) {
-//			String key = enumer.nextElement();
-//			String val = req.getHeader(key);
-//			System.out.println(key + " : " + val);
-//		}
-//	}
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("doGet call()");
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		
 		System.out.println("doPost call()");
 		String path = "c:/tmp";
-		
+
 		ServletContext sc = this.getServletContext();
-		path = sc.getRealPath("upload"); //웹서버가 실행되는 실제 주소위치로 변환 / 서버 상 실제경로
-		
+		path = sc.getRealPath("upload"); // 웹서버가 실행되는 실제 주소위치로 변환 / 서버 상 실제경로
+
 //		MultipartRequest multi = new MultipartRequest(요청정보, "저장위치", 파일사이즈, 인코딩타입(UTF-8));
 		MultipartRequest multi = new MultipartRequest(request, // 요청정보
-																							path , // 저장위치
+																							path, // 저장위치
 																							5 * 1024 * 1024, // 용량
 																							"UTF-8", // 인코딩
 																							new DefaultFileRenamePolicy());
 
-		Enumeration en = multi.getFileNames(); //올라간 파일의 이름 읽기
+		Enumeration en = multi.getFileNames(); // 올라간 파일의 이름 읽기
+		String author = multi.getParameter("author");
+		String title = multi.getParameter("title");
+		String fileN = null;
+		
 		while (en.hasMoreElements()) {
 			String name = (String) en.nextElement();
 			String fileName = multi.getFilesystemName(name);
+			fileN = fileName;
 			System.out.println("name : " + name + ", fileName" + fileName);
 		}
+		// 입력 후 저장 된 값 가져오기
+		FileDAO dao = new FileDAO();
+		FileVO vo = new FileVO();
+		vo.setAuthor(author);
+		vo.setFileName(fileN);
+		vo.setTitle(title);
+		FileVO rvo = dao.getInsertKeyVal(vo);
+		//가져온 값을 JSON 형식으로 생성. {"num" : ?, "author" : ? , "title" : ? , "day" : ?}
+		JSONObject obj = new JSONObject();
+		obj.put("num",rvo.getNum());
+		obj.put("author", rvo.getAuthor());
+		obj.put("title", rvo.getTitle());
+		obj.put("day", rvo.getDay());
+		obj.put("fileName", rvo.getFileName());
+		
+		response.getWriter().print(obj);
+		
+		
+		
 
 	}
 
